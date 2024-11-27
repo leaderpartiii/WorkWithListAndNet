03e05e31f4dc1567a8235ff8181a9cde13635127
Aizat <aliya.2005kaizat@gmail.com>
2024-11-27 23:59:41 +0300

    fixed to enum

--- app/src/main/java/com/example/workwithlistandnet/content/Internet.kt
+++ app/src/main/java/com/example/workwithlistandnet/content/Internet.kt
@@ -9,41 +9,43 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.example.workwithlistandnet.net.getImage
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.room.Room
 import coil3.compose.AsyncImage
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.load.resource.gif.GifDrawable
-import com.bumptech.glide.request.RequestListener
 import com.example.workwithlistandnet.R
+import com.example.workwithlistandnet.db.AppDatabase
+import com.example.workwithlistandnet.db.ImageDao
+import com.example.workwithlistandnet.db.ImageEntity
+import com.example.workwithlistandnet.net.getImage
 import kotlinx.coroutines.delay
-import com.example.workwithlistandnet.db.*
-import androidx.room.Room
 
 class Internet : AppCompatActivity() {
+    enum class State {
+        Error,
+        Pause,
+        LoadingRequest,
+        LoadingImage,
+        Success
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { MainWindow() }
@@ -57,41 +59,30 @@ class Internet : AppCompatActivity() {
             AppDatabase::class.java, "database-name"
         ).allowMainThreadQueries().build()
         val imageDao = db.imageDao()
-        val imagesList = GetImagesFromDB(imageDao)
+        val imagesList = getImagesFromDB(imageDao)
 
 
-        val error = rememberSaveable { mutableIntStateOf(0) }
-        val loadingRequest = rememberSaveable { mutableStateOf(false) }
-        val loadingImage =
-            rememberSaveable { mutableStateOf(imagesList.isNotEmpty()) }
-        val onSuccess = rememberSaveable { mutableStateOf(false) }
+        val currState =
+            rememberSaveable { mutableStateOf(if (imagesList.isEmpty()) State.Pause else State.LoadingImage) }
 
         val listState = rememberLazyListState()
 
-
+        LoadImage(imagesList, currState)
         AddImageToDB(imageDao, imagesList)
-        LoadImage(imagesList, error, loadingRequest, loadingImage)
-
 
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NewImage(error, loadingRequest, loadingImage, onSuccess)
-            ResetButton(
-                result = imagesList,
-                error = error,
-                loadingRequest = loadingRequest,
-                loadingImage = loadingImage,
-                imageDao
-            )
-            if (error.intValue == 0 && (loadingImage.value || loadingRequest.value) && !onSuccess.value)
-                LoadingWithDots()
-            else if (error.intValue == 0 && (!loadingImage.value && !loadingRequest.value) && onSuccess.value)
-                Text(text = "Вот что именно получилось")
-            else if (error.intValue != 0)
-                Text(text = "Что-то не очень получилось")
+            NewImage(currState)
+            ResetButton(currState, imagesList, imageDao)
+            when (currState.value) {
+                State.LoadingImage, State.LoadingRequest -> LoadingWithDots()
+                State.Success -> Text(text = "Вот что именно получилось")
+                State.Error -> Text(text = "Что-то не очень получилось")
+                State.Pause -> Text(text = "Ждем инструкции")
+            }
 
             LazyColumn(
                 state = listState,
@@ -101,11 +92,8 @@ class Internet : AppCompatActivity() {
                 items(imagesList) { imageUrl ->
                     DisplayContent(
                         imageUrl,
-                        error,
-                        loadingRequest,
-                        loadingImage,
-                        imagesList.size,
-                        onSuccess
+                        currState,
+                        imagesList.size
                     )
                 }
             }
@@ -138,37 +126,28 @@ class Internet : AppCompatActivity() {
     @Composable
     fun LoadImage(
         result: SnapshotStateList<String>,
-        error: MutableIntState,
-        loadingRequest: MutableState<Boolean>,
-        loadingImage: MutableState<Boolean>,
+        currState: MutableState<State>,
     ) {
-        LaunchedEffect(loadingRequest.value) {
-            if (loadingRequest.value)
+        LaunchedEffect(currState.value) {
+            if (currState.value == State.LoadingRequest)
                 try {
-                    val image = getImage()
-                    result.add(image)
+                    result.add(getImage())
+                    currState.value = State.LoadingImage
                 } catch (e: Exception) {
-                    error.intValue = 1
-                } finally {
-                    loadingRequest.value = false
-                    loadingImage.value = true
+                    currState.value = State.Error
                 }
         }
     }
 
     @Composable
     fun ResetButton(
+        currState: MutableState<State>,
         result: SnapshotStateList<String>,
-        error: MutableIntState,
-        loadingRequest: MutableState<Boolean>,
-        loadingImage: MutableState<Boolean>,
         imageDao: ImageDao,
     ) {
         Button(onClick = {
+            currState.value = State.Pause
             result.clear()
-            error.intValue = 0
-            loadingRequest.value = true
-            loadingImage.value = false
             imageDao.clearImages()
         }) {
             Text(text = "Сбросить всё")
@@ -186,7 +165,7 @@ class Internet : AppCompatActivity() {
                 kotlinx.coroutines.withContext(kotlinx.coroutines.Dispatchers.IO) {
                     try {
                         imageDao.insertImage(newImage)
-                    } catch (e: Exception) {
+                    } catch (_: Exception) {
                     }
                 }
             }
@@ -194,7 +173,7 @@ class Internet : AppCompatActivity() {
     }
 
     @Composable
-    fun GetImagesFromDB(imageDao: ImageDao): SnapshotStateList<String> {
+    fun getImagesFromDB(imageDao: ImageDao): SnapshotStateList<String> {
         val imagesList = remember { mutableStateListOf<String>() }
 
         LaunchedEffect(Unit) {
@@ -202,7 +181,7 @@ class Internet : AppCompatActivity() {
                 try {
                     val images = imageDao.getAllImages()
                     imagesList.addAll(images.map { it.url })
-                } catch (e: Exception) {
+                } catch (_: Exception) {
                 }
             }
         }
@@ -213,102 +192,43 @@ class Internet : AppCompatActivity() {
 
     @Composable
     fun NewImage(
-        error: MutableIntState,
-        loadingRequest: MutableState<Boolean>,
-        loadingImage: MutableState<Boolean>,
-        onSuccess: MutableState<Boolean>,
+        currState: MutableState<State>
     ) {
         Button(onClick = {
-            error.intValue = 0
-            loadingRequest.value = true
-            loadingImage.value = false
-            onSuccess.value = false
+            currState.value = State.LoadingRequest
         }) {
-            Text(text = "Перегенерировать вайфу")
-        }
-    }
-
-    @Composable
-    fun DisplayImage(gifUrl: String, loadingImage: MutableState<Boolean>) {
-        AndroidView(
-            factory = { context ->
-                ImageView(context).apply {
-                    scaleType = ImageView.ScaleType.CENTER_CROP
-                    Glide.with(this)
-                        .asGif()
-                        .load(gifUrl)
-                        .listener(loadGifListener(loadingImage))
-                        .into(this)
-                }
-            },
-            modifier = Modifier.size(200.dp)
-        )
-    }
-
-    private fun loadGifListener(loadingImage: MutableState<Boolean>): RequestListener<GifDrawable> {
-        return object : RequestListener<GifDrawable> {
-
-            override fun onLoadFailed(
-                e: GlideException?,
-                model: Any?,
-                target: com.bumptech.glide.request.target.Target<GifDrawable>?,
-                isFirstResource: Boolean
-            ): Boolean {
-                return false
-            }
-
-            override fun onResourceReady(
-                resource: GifDrawable?,
-                model: Any?,
-                target: com.bumptech.glide.request.target.Target<GifDrawable>?,
-                dataSource: DataSource?,
-                isFirstResource: Boolean
-            ): Boolean {
-                loadingImage.value = false;
-                return false
-            }
+            Text(text = "Сгенерировать вайфу")
         }
     }
 
     @Composable
     fun DisplayContent(
         result: String,
-        error: MutableIntState,
-        loadingRequest: MutableState<Boolean>,
-        loadingImage: MutableState<Boolean>,
+        currState: MutableState<State>,
         size: Int,
-        onSuccess: MutableState<Boolean>
     ) {
-        if (error.intValue == 0) {
-            when {
-                loadingRequest.value -> {
-                    DisplayLoadingGif()
-                }
-
-                loadingImage.value && size != 0 -> {
-                    DisplayLoadingGif()
-                    AsyncImage(
-                        model = result,
-                        contentDescription = null,
-                        onSuccess = { onSuccess.value = true;loadingImage.value = false; },
-                        onError = { error.intValue = 1 }
-                    )
-                }
-
-                result.isNotEmpty() -> {
-                    AsyncImage(
-                        model = result,
-                        contentDescription = null,
-                        onSuccess = { onSuccess.value = true;loadingImage.value = false; },
-                        onError = { error.intValue = 1 }
-                    )
-                }
-            }
-        } else {
+        if (currState.value == State.Error) {
             Image(
                 painter = painterResource(id = R.drawable.cat),
                 contentDescription = null
             )
+            return
+        }
+        when {
+
+            currState.value == State.LoadingRequest && size == 0 -> {
+                DisplayLoadingGif()
+            }
+
+            else -> {
+
+                AsyncImage(
+                    model = result,
+                    contentDescription = null,
+                    onSuccess = { currState.value = State.Success },
+                    onError = { currState.value = State.Error }
+                )
+            }
         }
     }
 
